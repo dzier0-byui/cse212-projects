7aea3893bc3d057a5e6d4af5f4520553c2c98468
dzier0-byui <dzi24004@byui.edu>
2025-07-12 15:10:30 -0600

    week 02 assignment

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 // TODO Problem 2 - Write and run test cases and fix the code to match requirements.
@@ -6,23 +7,36 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Add multiple items with different priorities and dequeue one item
+    // Expected Result: Volleyball should be returned
+    // Defect(s) Found: None
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue("Basketball", 6);
+        priorityQueue.Enqueue("Volleyball", 10);
+        priorityQueue.Enqueue("Soccer", 6);
+        priorityQueue.Enqueue("Pickleball", 9);
+
+        var result = priorityQueue.Dequeue();
+        Assert.AreEqual("Volleyball", result);
+
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Add multiple items with the same priority and ensure the first one added is dequeued first
+    // Expected Result: Basketball should be returned
+    // Defect(s) Found: None
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue("Basketball", 6);
+        priorityQueue.Enqueue("Soccer", 6);
+
+        var result = priorityQueue.Dequeue();
+        Assert.AreEqual("Basketball", result);
     }
 
     // Add more test cases as needed below.
